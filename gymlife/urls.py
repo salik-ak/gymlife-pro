258629d0aaf239073cb04993327cfd5be93304f8
salik ak <saleekkariyad@gmai.com>
2023-07-26 21:16:45 +0530

    static editss

--- gymlife/urls.py
+++ gymlife/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
@@ -28,5 +27,4 @@ urlpatterns = [
     path('adminpanel/', include('adminpanel.urls')),
     path('trainer/', include('trainer.urls')),
     path('course/', include('course.urls')),
-]
-urlpatterns += staticfiles_urlpatterns()
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
