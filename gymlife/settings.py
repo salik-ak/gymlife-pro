a120f5dfa602edb143801286f4c3402ac96b7455
salik ak <saleekkariyad@gmai.com>
2023-07-27 17:45:40 +0530

    static6

--- gymlife/settings.py
+++ gymlife/settings.py
@@ -123,10 +123,11 @@ USE_L10N = True
 USE_TZ = True
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS =[
-    os.path.join(BASE_DIR,'static')
-]
+# STATICFILES_DIRS =[
+#     BASE_DIR /'staticfiles'
+# ]
 
 
 MEDIA_ROOT = BASE_DIR /'uploads'
