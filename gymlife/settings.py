d7969fe2e49067318782b098bca9c52df823385b
salik ak <saleekkariyad@gmai.com>
2023-07-27 13:13:05 +0530

    static

--- gymlife/settings.py
+++ gymlife/settings.py
@@ -123,7 +123,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_ROOT = BASE_DIR/'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 # STATICFILES_DIRS =[
 #     BASE_DIR /'static'
