b78888d2f26541cc385d32d8d78b2480ea827398
salik ak <saleekkariyad@gmai.com>
2023-07-27 12:45:23 +0530

    static root 2

--- gymlife/settings.py
+++ gymlife/settings.py
@@ -15,6 +15,7 @@ from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -122,15 +123,12 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR / 'static')
 STATICFILES_DIRS =[
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR / 'static')
 ]
+
 
 MEDIA_ROOT = BASE_DIR /'uploads'
 MEDIA_URL = '/media/'
