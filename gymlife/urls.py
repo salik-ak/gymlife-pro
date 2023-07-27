de55a80ab7978f293506a6231d1d3be5e7582a23
salik ak <saleekkariyad@gmai.com>
2023-07-27 17:40:41 +0530

    static5

--- gymlife/urls.py
+++ gymlife/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('adminpanel/', include('adminpanel.urls')),
     path('trainer/', include('trainer.urls')),
     path('course/', include('course.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
