9d86e3c1cc717e4ac457cd56cdacae3de2facd3d
salik ak <saleekkariyad@gmai.com>
2023-07-27 11:00:20 +0530

    load static32

--- gymlife/settings.py
+++ gymlife/settings.py
@@ -127,9 +127,11 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS =[
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS =[
+#     os.path.join(BASE_DIR, 'static')
+# ]
+STATICFILES_DIRS = [BASE_DIR / "static"] # new
+# STATIC_ROOT = BASE_DIR / "staticfiles" # new
 
 MEDIA_ROOT = BASE_DIR /'uploads'
 MEDIA_URL = '/media/'
